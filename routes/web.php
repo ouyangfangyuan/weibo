9c8dc79c8fc42539a37027aa4e53c127a5c89bff
ouyangfy <824029856@qq.com>
2019-07-03 15:06:00 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 
+Route::get('signup','UserController@create')->name('signup');
+
+
 
